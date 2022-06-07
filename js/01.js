ae2c764257904e372e77fd9db8359bacdf48f88e
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-06-06 21:28:29 -0400

    Methods y v-on:click

--- js/01.js
+++ js/01.js
@@ -2,12 +2,20 @@
 const app = new Vue({
     //Detectar id a trabajar con Vue.js
     el: '#app',
-    data:{
-        titulo:'Hola mundo con vue',
-        frutas:[
-            {nombre:'Manzana',cantidad:10},
-            {nombre:'Pera',cantidad:0},
-            {nombre:'Platano',cantidad:20},
-        ]
+    data: {
+        titulo: 'Hola mundo con vue',
+        frutas: [
+            {nombre: 'Manzana', cantidad: 10},
+            {nombre: 'Pera', cantidad: 5},
+            {nombre: 'Platano', cantidad: 20}
+        ],
+        nuevaFruta: ''
+    },
+    methods:{
+        agregarFruta (){
+            this.frutas.push({//Para acceder a las propiedades que estan dentro de cualquier metodo
+                nombre: this.nuevaFruta, cantidad: 0
+            })
+        }
     }
 })
