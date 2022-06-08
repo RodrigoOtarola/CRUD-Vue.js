fe90bed8e6a5d8305c0b87e210a7d30c170b9e6a
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-06-07 22:34:11 -0400

    Creacion CRUD

---
+++ CRUD-LocalStorage/app.js
@@ -0,0 +1,19 @@
+//Instanciar Vue
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'GYM con Vue',
+        tareas: [],
+        nuevaTarea: ''
+    },
+    methods:{
+        agregarTarea: function (){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false
+            });
+            this.nuevaTarea = '';
+        }
+
+    }
+})
