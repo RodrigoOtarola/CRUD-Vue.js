cc3b4fa83dd8eacdb51c11354c023e43a7b39d63
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2022-06-08 21:33:54 -0400

    tabla con localStorage

--- CRUD-LocalStorage/app.js
+++ CRUD-LocalStorage/app.js
@@ -6,17 +6,32 @@ const app = new Vue({
         tareas: [],
         nuevaTarea: ''
     },
-    methods:{
-        agregarTarea: function (){
+    methods: {
+        agregarTarea: function () {
             this.tareas.push({
                 nombre: this.nuevaTarea,
                 estado: false
             });
             this.nuevaTarea = '';
+            //set, setea el valor de item.
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
         },
-        editarTarea: function (index){
+        editarTarea: function (index) {
             this.tareas[index].estado = true;
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
+        },
+        eliminarTarea: function (index) {
+            this.tareas.splice(index, 1)//splice, para eliminar
+            localStorage.setItem('gym-vue', JSON.stringify(this.tareas));
+        },
+    },
+    //Metodo created.
+    created: function () {
+        let datosDB = JSON.parse(localStorage.getItem('gym-vue'));
+        if (datosDB === null) {
+            this.tareas = [];
+        } else {
+            this.tareas = datosDB;
         }
-
     }
 })
